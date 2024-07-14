ab8eaa7791a53062e00a5fa083a98f3b6d54f5fc
Miguelangel Leonet <leonet.miguelangel.2021@gmail.com>
2024-07-14 00:10:37 -0400

    config final

--- resources/views/Paginas/Coordinadores/Materias.blade.php
+++ resources/views/Paginas/Coordinadores/Materias.blade.php
@@ -21,11 +21,11 @@
                 @foreach ($materias as $materia)
                     <tr>
                         <td class="py-3 px-6 border-b border-gray-200">{{ $materia->id }}</td>
-                        <td class="py-3 px-6 border-b border-gray-200">{{ $materia->grado->año }}</td>
+                        <td class="py-3 px-6 border-b border-gray-200">{{ $materia->grado->nombre_grado }}</td>
                         <td class="py-3 px-6 border-b border-gray-200">{{ $materia->nombre }}</td>
                         <td class="py-3 px-6 text-center border-b border-gray-200">
                             <a href="#" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded text-xs editarMateriaBtn"
-                               data-id="{{ $materia->id }}" data-grado="{{ $materia->grado->año }}" data-nombre="{{ $materia->nombre }}">
+                               data-id="{{ $materia->id }}" data-grado="{{ $materia->grado->nombre_grado }}" data-nombre="{{ $materia->nombre }}">
                                 <i class="fas fa-edit mr-1"></i> Editar
                             </a>
                         </td>
@@ -45,7 +45,7 @@
                         <label for="addGrado" class="block mb-2">Grado:</label>
                         <select id="addGrado" name="grado_id" class="border border-gray-300 px-2 py-1 rounded mb-2">
                             @foreach($grados as $grado)
-                                <option value="{{ $grado->id }}">{{ $grado->año }}</option>
+                                <option value="{{ $grado->id }}">{{ $grado->nombre_grado }}</option>
                             @endforeach
                         </select>
                     </div>
