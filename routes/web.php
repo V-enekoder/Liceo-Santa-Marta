ca0ff0210cc76bea9fee62420654675db4769640
Victor <vjxoen@gmail.com>
2024-07-16 14:01:47 -0400

    Rutas organizadas

--- routes/web.php
+++ routes/web.php
@@ -58,16 +58,9 @@ Route::get('/dashboard/dataDocentes', [CoordinadorController::class, 'modificarD
 //rutas para la edición y creación de materias (coordinador)
 Route::get('/dashboard/dataMaterias', [MateriaController::class, 'mostrarMaterias'])->name('sidebar.materias');
 Route::post('/dashboard/dataMaterias', [MateriaController::class, 'crear_materia'])->name('sidebar.crearMateria');
-<<<<<<< HEAD
 //nuevas rutas
 Route::put('/dashboard/dataMaterias/{id}', [MateriaController::class, 'editar_materia'])->name('sidebar.editarMateria');
 Route::delete('/dashboard/dataMaterias/{id}', [MateriaController::class, 'eliminar_materia'])->name('sidebar.eliminarMateria');
-=======
-Route::put('/dashboard/dataMaterias/{id}', [MateriaController::class, 'editar_materia'])->name('sidebar.editarMateria');
-Route::delete('/dashboard/dataMaterias/{id}', [MateriaController::class, 'eliminar_materia'])->name('sidebar.eliminarMateria');
-
-
->>>>>>> migue2
 
 
 Route::get('/dashboard/asignar-carga', [DocenteController::class, 'mostrarFormularioAsignarCarga'])
