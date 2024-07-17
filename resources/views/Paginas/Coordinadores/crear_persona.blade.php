ae9c6a201608b2b2c454c9e9e997f72036781dfe
xxadreww <francisco30293487@gmail.com>
2024-07-17 16:13:14 -0400

    crear persoan vista lista y agregada al sidebar

--- resources/views/Paginas/Coordinadores/crear_persona.blade.php
+++ resources/views/Paginas/Coordinadores/crear_persona.blade.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Crear Usuario</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-
-<body>
-    <div class="container mt-5">
-        <h1>Crear Usuario</h1>
+<x-app-layout>
 
         @if ($errors->any())
             <div class="alert alert-danger">
@@ -22,16 +10,23 @@
             </div>
         @endif
 
+
+        <h1 class="h1Docente">Coordinador: Crear persona</h1>
+
+        <div class="panel panel-default">
+            <div class="panel-heading text-table-head">Panel de registro</div>
+            <div class="panel-body">
+
         <form action="{{ route('sidebar.crearpersona') }}" method="POST">
             @csrf
             <div class="form-group">
-                <label for="cedula">Cédula</label>
+                <label class="text-default-black" for="cedula">Cédula</label>
                 <input type="text" class="form-control" id="cedula" name="cedula" value="{{ old('cedula') }}"
                     required>
             </div>
 
             <div class="form-group">
-                <label for="categoria_id">categoria</label>
+                <label class="text-default-black" for="categoria_id">Categoria</label>
                 <select class="form-control" id="categoria_id" name="categoria_id" required>
                     <option value="">Seleccione una categoria</option>
                     <option value="1">Usuario</option>
@@ -40,7 +35,7 @@
             </div>
 
             <div class="form-group" id="rol_id_group">
-                <label for="rol_id">Rol</label>
+                <label class="text-default-black" for="rol_id">Rol</label>
                 <select class="form-control" id="rol_id" name="rol_id" required>
                     <option value="">Seleccione un rol</option>
                     @foreach ($roles as $rol)
@@ -50,53 +45,53 @@
             </div>
 
             <div class="form-group" id="email_group">
-                <label for="email">Email</label>
+                <label class="text-default-black" for="email">Email</label>
                 <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
             </div>
 
             <div class="form-group" id="password_group">
-                <label for="password">Contraseña</label>
+                <label class="text-default-black" for="password">Contraseña</label>
                 <input type="password" class="form-control" id="password" name="password">
             </div>
 
             <div class="form-group" id="password_confirmation_group">
-                <label for="password_confirmation">Confirmar Contraseña</label>
+                <label class="text-default-black" for="password_confirmation">Confirmar Contraseña</label>
                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation">
             </div>
 
             <div class="form-group">
-                <label for="primer_nombre">Primer Nombre</label>
+                <label class="text-default-black" for="primer_nombre">Primer Nombre</label>
                 <input type="text" class="form-control" id="primer_nombre" name="primer_nombre"
                     value="{{ old('primer_nombre') }}" required>
             </div>
 
             <div class="form-group">
-                <label for="segundo_nombre">Segundo Nombre</label>
+                <label class="text-default-black" for="segundo_nombre">Segundo Nombre</label>
                 <input type="text" class="form-control" id="segundo_nombre" name="segundo_nombre"
                     value="{{ old('segundo_nombre') }}">
             </div>
 
             <div class="form-group">
-                <label for="primer_apellido">Primer Apellido</label>
+                <label class="text-default-black" for="primer_apellido">Primer Apellido</label>
                 <input type="text" class="form-control" id="primer_apellido" name="primer_apellido"
                     value="{{ old('primer_apellido') }}" required>
             </div>
 
             <div class="form-group">
-                <label for="segundo_apellido">Segundo Apellido</label>
+                <label class="text-default-black" for="segundo_apellido">Segundo Apellido</label>
                 <input type="text" class="form-control" id="segundo_apellido" name="segundo_apellido"
                     value="{{ old('segundo_apellido') }}">
             </div>
 
             <div class="form-group">
-                <label for="direccion">Dirección</label>
+                <label class="text-default-black" for="direccion">Dirección</label>
                 <input type="text" class="form-control" id="direccion" name="direccion"
                     value="{{ old('direccion') }}">
             </div>
 
             <div id="estudiante-fields" style="display: none;">
                 <div class="form-group">
-                    <label for="ultimo_grado_aprobado">Último Grado Aprobado</label>
+                    <label class="text-default-black" for="ultimo_grado_aprobado">Último Grado Aprobado</label>
                     <select class="form-control" id="ultimo_grado_aprobado" name="ultimo_grado_aprobado">
                         <option value="">Seleccione el último grado aprobado</option>
                         @for ($i = 0; $i <= 4; $i++)
@@ -108,10 +103,11 @@
                 </div>
             </div>
 
-            <button type="submit" class="btn btn-primary">Crear Usuario</button>
+            <button type="submit"
+            class="mt-5 mb-0 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Crear usuario</button>
         </form>
     </div>
-</body>
+
 
 <script>
     var categoriaSelect = document.getElementById('categoria_id');
@@ -141,4 +137,4 @@
     });
 </script>
 
-</html>
+</x-app-layout>
