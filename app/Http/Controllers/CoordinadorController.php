70502b4522dd37a8c69f3eac11d157838ddf8667
Victor <vjxoen@gmail.com>
2024-07-17 23:40:48 -0400

    Comprobando no haber roto nada

--- app/Http/Controllers/CoordinadorController.php
+++ app/Http/Controllers/CoordinadorController.php
@@ -26,8 +26,8 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\ValidationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
-
-
+use Exception;
+use App\Models\Persona;
 
 class CoordinadorController extends Controller{
 
@@ -43,35 +43,7 @@ class CoordinadorController extends Controller{
         //Gate::authorize('modificar_estudiante');
         return view('Paginas.Coordinadores.modificacion_estudiantes',);
     }
-//-----------------------------------------------------------------------------------------------------------------------
-    // public function mostrarDocentes() 
-    // {
-    // // Obtener todos los docentes junto con la información del usuario
-    // $docentes = Docente::with('user.persona')->get();
-    // return view('Paginas.Coordinadores.Profesores', ['docentes' => $docentes]);
-    // }
 
-    // public function updateDocente(Request $request, $id)
-    // {
-    //     $docente = Docente::find($id);
-
-    //     $request->validate([
-    //         'primer_nombre' => 'required|string|max:255',
-    //         'primer_apellido' => 'required|string|max:255',
-    //         'cedula' => 'required|string|max:255',
-    //         'email' => 'required|email|max:255',
-    //     ]);
-
-    //     $docente->user->persona->primer_nombre = $request->input('primer_nombre');
-    //     $docente->user->persona->primer_apellido = $request->input('primer_apellido');
-    //     $docente->user->persona->cedula = $request->input('cedula');
-    //     $docente->user->email = $request->input('email');
-    //     $docente->user->persona->save();
-    //     $docente->user->save();
-
-    //     return response()->json(['message' => 'Docente actualizado correctamente', 'docente' => $docente]);
-    // }
-//-----------------------------------------------------------------------------------------------------------------------
     function modificarMaterias(){
         //Gate::authorize('modificar_materias');
         return view('Paginas.Coordinadores.Materias',);
