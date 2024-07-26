879c8ee5a97e9320469a32f1fb856b435bbe9b66
Victor <vjxoen@gmail.com>
2024-07-26 15:53:54 -0400

    Se agregó el sidebar, pero no funciona con la logica, por lo cual se ocultó.

---
+++ resources/views/layouts/simple.blade.php
@@ -0,0 +1,15 @@
+<!-- layouts/simple.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Layout</title>
+</head>
+
+<body>
+    @yield('content')
+</body>
+
+</html>
